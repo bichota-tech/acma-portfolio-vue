079c99f28a3e10467a61f7fb41a398708dc1cc14
bichota-tech <adalvarez2021@gmail.com>
2026-06-10 13:58:58 +0200

    feat: implement 3D carousel component with autoplay and touch support

---
+++ src/components/layout/AppCarousel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="carousel-wrapperfame">
+    <div class="carouselfame" id="carousel">
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
+  
